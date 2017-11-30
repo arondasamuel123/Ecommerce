3a287fcfbcb05694f69794a25501681af51a3530
PeterBaraka <peter.opiyo@strathmore.edu>
2017-11-30 11:22:24 +0300

    Second Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading" align="center">Dashboard</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,8 +14,11 @@
                         </div>
                     @endif
 
-                    Welcome to BootsForU! Click the button to get started
+                    <p align="center">Welcome to BootsForU!</p>
+                    <p align="center">Click the button to get started</p>
+                        <div align="center">
                     <a href="{{ url ('/home1') }}" class="btn btn-primary">HOME</a>
+                        </div>
                 </div>
             </div>
         </div>
